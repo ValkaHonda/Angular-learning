11bff9f8e4a00bfda70c0e8bf4a7403ee6b48233
Lenny <valentin805@gmail.com>
2018-07-25 23:00:55 +0300

    Add control menu bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ArtComponent } from './articleList/articleList.component';
+import { ControlMenuComponent } from './controls/control-menu/control-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ArtComponent
+    ArtComponent,
+    ControlMenuComponent
   ],
   imports: [
     BrowserModule
